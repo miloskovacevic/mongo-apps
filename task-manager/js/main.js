251f837b0cc75d861a548d60ad1ba851fa6cae83
Milos <miloscpt@gmail.com>
2016-01-15 21:11:23 +0100

    Edit, delete: done

--- task-manager/js/main.js
+++ task-manager/js/main.js
@@ -1,12 +1,13 @@
-function getTasks(){
-    console.log('tasks...');
-}
-
-
 $(document).ready(function () {
-    $("#add_category").submit(addCategory);
+    $('#add_category').submit(addCategory);
+    $('#edit_category').submit(updateCategory);
+    $('body').on('click', '.btn-edit-category',  setCategory);
+    $('body').on('click', '.btn-delete-category', deleteCategory);
 });
 
+function getTasks(){
+
+}
 
 var api_key = 'xQwEfYp4PIRcRUGVunieXhPEvHv3ZUUm';
 
@@ -15,11 +16,9 @@ function getCategories(){
     $.get('https://api.mongolab.com/api/1/databases/taskmanager/collections/categories?apiKey=' + api_key, function (podaci) {
         var output = '<ul class="list-group">';
         $.each(podaci, function (key, pojedinacan_podatak) {
-            output += '<li class="list-group-item category">' + pojedinacan_podatak.category_name + '<div class="pull-right">' +
-                '<a class="btn btn-primary btn-edit-category" data-category-id="'+ pojedinacan_podatak._id.$oid + '">Edit</a>' +
-                '<a class="btn btn-danger btn-delete-category" data-category-id="'+ pojedinacan_podatak._id.$oid + '">Delete</a></div>' + '</li>';
+            output += '<li class="list-group-item category">' + pojedinacan_podatak.category_name + '<div class="pull-right"><a class="btn btn-primary btn-edit-category" data-category-id="' + pojedinacan_podatak._id.$oid + '">Edit</a><a class="btn btn-danger btn-delete-category" data-category-id="'+ pojedinacan_podatak._id.$oid + '">Delete</a></div>' + '</li>';
         });
-        output += '</ul';
+        output += '</ul>';
         $("#categories").html(output);
     });
 }
@@ -39,4 +38,55 @@ function addCategory(){
         }
     });
     return false;
+}
+
+function updateCategory(){
+    var category_name = $("#category_name").val();
+    var category_id = sessionStorage.getItem('currentCategoryId');
+
+    $.ajax({
+        url: "https://api.mongolab.com/api/1/databases/taskmanager/collections/categories/" + category_id + "?apiKey=" + api_key,
+        data: JSON.stringify({"category_name" : category_name}),
+        type: "PUT",
+        contentType: "application/json",
+        success: function(data){
+            window.location.href = "categories.html";
+        },
+        error: function(xhr, status, err){
+            console.log(err);
+        }
+    });
+    return false;
+}
+
+function setCategory(){
+    var category_id = $(this).data('category-id');
+    sessionStorage.setItem('currentCategoryId', category_id);
+    window.location.href = 'editcategory.html';
+    return false;
+}
+
+function deleteCategory(){
+    var category_id = $(this).data('category-id');
+
+    $.ajax({
+        url: "https://api.mongolab.com/api/1/databases/taskmanager/collections/categories/" + category_id + "?apiKey=" + api_key,
+        type: "DELETE",
+        async: true,
+        success: function(data){
+            window.location.href = "categories.html";
+        },
+        error: function(xhr, status, err){
+            console.log(err);
+        }
+    });
+    return false;
+}
+
+function getCategory(){
+    var category_id = sessionStorage.getItem('currentCategoryId');
+
+    $.get("https://api.mongolab.com/api/1/databases/taskmanager/collections/categories/" + category_id + "?apiKey=" + api_key, function (podatak) {
+        $("#category_name").val(podatak.category_name);
+    })
 }
